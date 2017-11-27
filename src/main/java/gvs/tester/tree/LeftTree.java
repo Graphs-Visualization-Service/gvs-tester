de764d3c570c656596bf6f73f860aa58334d8010
Murièle Trentini <muriele@trentini.ch>
2017-11-27 11:15:03 +0100

    update tree tests

--- src/main/java/gvs/tester/tree/LeftTree.java
+++ src/main/java/gvs/tester/tree/LeftTree.java
@@ -6,11 +6,11 @@ import java.util.Map;
 import gvs.model.tree.TestBinaryNode;
 import gvs.tree.GVSTreeWithCollection;
 
-public class RightTree {
+public class LeftTree {
   static Map<String, TestBinaryNode> nodes = new HashMap<>();
 
   public static void main(String[] args) {
-    GVSTreeWithCollection gvsTree = new GVSTreeWithCollection("Right Tree");
+    GVSTreeWithCollection gvsTree = new GVSTreeWithCollection("Left Tree");
     gvsTree.display();
     initNodes();
     nodes.values().forEach(node -> gvsTree.add(node));
@@ -26,10 +26,10 @@ public class RightTree {
     nodes.put("5", new TestBinaryNode("5", null));
     nodes.put("6", new TestBinaryNode("6", null));
     
-    nodes.get("1").setRightChild(nodes.get("2"));
-    nodes.get("2").setRightChild(nodes.get("3"));
-    nodes.get("3").setRightChild(nodes.get("4"));
-    nodes.get("4").setRightChild(nodes.get("5"));
-    nodes.get("5").setRightChild(nodes.get("6"));
+    nodes.get("1").setLeftChild(nodes.get("2"));
+    nodes.get("2").setLeftChild(nodes.get("3"));
+    nodes.get("3").setLeftChild(nodes.get("4"));
+    nodes.get("4").setLeftChild(nodes.get("5"));
+    nodes.get("5").setLeftChild(nodes.get("6"));
   }
 }
--- src/main/java/gvs/tester/tree/MultipleRoots.java
+++ src/main/java/gvs/tester/tree/MultipleRoots.java
@@ -1,28 +1,49 @@
 package gvs.tester.tree;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import gvs.model.tree.TestBinaryNode;
 import gvs.tree.GVSTreeWithCollection;
 
 public class MultipleRoots {
-  static List<TestBinaryNode> nodes = new ArrayList<>();
+  static Map<String, TestBinaryNode> nodes = new HashMap<>();
 
   public static void main(String[] args) {
-    initNodes();
     GVSTreeWithCollection gvsTree = new GVSTreeWithCollection("Multiple Roots Tree");
-    nodes.forEach(node -> gvsTree.add(node));
+//    initNodes(1, 5);
+//    nodes.values().forEach(node -> gvsTree.add(node));
+//    gvsTree.display();
+//    nodes.clear();
+    initNodes(10, 20);
+    nodes.values().forEach(node -> gvsTree.add(node));
     gvsTree.display();
     gvsTree.disconnect();
   }
 
-  private static void initNodes() {
-    TestBinaryNode root1 = new TestBinaryNode("1", null);
-    TestBinaryNode root2 = new TestBinaryNode("2", null);
-    TestBinaryNode root3 = new TestBinaryNode("3", null);
-    nodes.add(root1);
-    nodes.add(root2);
-    nodes.add(root3);
+  private static void initNodes(int start, int end) {
+    for (int i = start; i < end; i++) {
+      TestBinaryNode node = createOrGetNode(i);
+      TestBinaryNode leftChild = createOrGetNode(2 * i);
+      TestBinaryNode rightChild = createOrGetNode(2 * i + 1);
+      node.setLeftChild(leftChild);
+      node.setRightChild(rightChild);
+    }
+  }
+
+  private static TestBinaryNode createOrGetNode(int i) {
+    TestBinaryNode node = nodes.get(Integer.toString(i));
+    if (node == null) {
+      node = createNode(i);
+    }
+    return node;
+  }
+
+  private static TestBinaryNode createNode(int i) {
+    TestBinaryNode node = new TestBinaryNode(Integer.toString(i), null);
+    nodes.put(node.getNodeLabel(), node);
+    return node;
   }
 }
