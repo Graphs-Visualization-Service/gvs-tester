205a2c85a952e115e5020abcdb73b36d95238e8a
Murièle Trentini <muriele@trentini.ch>
2017-12-02 16:48:48 +0100

    fix same labels -> was a problem with a bad overriding of hashCode and Equals

--- src/main/java/gvs/model/tree/TestBinaryNode.java
+++ src/main/java/gvs/model/tree/TestBinaryNode.java
@@ -47,31 +47,6 @@ public class TestBinaryNode implements GVSBinaryTreeNode {
   }
 
   @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((label == null) ? 0 : label.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    TestBinaryNode other = (TestBinaryNode) obj;
-    if (label == null) {
-      if (other.label != null)
-        return false;
-    } else if (!label.equals(other.label))
-      return false;
-    return true;
-  }
-
-  @Override
   public String toString() {
     return label;
   }
---
+++ src/main/java/gvs/tester/graph/SameLabels.java
@@ -0,0 +1,59 @@
+package gvs.tester.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gvs.graph.GVSDefaultVertex;
+import gvs.graph.GVSDirectedEdge;
+import gvs.graph.GVSGraph;
+import gvs.graph.GVSUndirectedEdge;
+import gvs.model.graph.TestDefaultVertex;
+import gvs.model.graph.TestDirectedEdge;
+import gvs.model.graph.TestUndirectedEdge;
+import gvs.styles.GVSColor;
+import gvs.styles.GVSLineStyle;
+import gvs.styles.GVSLineThickness;
+import gvs.styles.GVSStyle;
+
+public class SameLabels {
+
+  private static List<GVSDefaultVertex> vertices = new ArrayList<>();
+  private static List<GVSDirectedEdge> directedEdges = new ArrayList<>();
+  private static List<GVSUndirectedEdge> undirectedEdges = new ArrayList<>();
+
+  public static void main(String[] args) {
+    GVSGraph graph = new GVSGraph("Default Graph");
+    createVertices(graph);
+    createEdges(graph);
+    graph.display();
+    graph.disconnect();
+  }
+
+  private static void createEdges(GVSGraph graph) {
+    int j = 1;
+    for (int i = 0; i < 5; i++) {
+      if (i % 2 == 0) {
+        TestDirectedEdge e = new TestDirectedEdge(vertices.get(i),
+            vertices.get(j), i + "");
+        directedEdges.add(e);
+        graph.add(e);
+      } else {
+        TestUndirectedEdge e = new TestUndirectedEdge(vertices.get(i),
+            vertices.get(j), i + "");
+        undirectedEdges.add(e);
+        graph.add(e);
+      }
+      j++;
+      j %= 4;
+    }
+  }
+
+  private static void createVertices(GVSGraph graph) {
+    for (int i = 0; i < 5; i++) {
+      TestDefaultVertex v = new TestDefaultVertex("v");
+      vertices.add(v);
+      graph.add(v);
+    }
+  }
+
+}
