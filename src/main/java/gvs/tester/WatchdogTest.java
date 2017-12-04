68ee89b0a6714344778fa253b491dfaef302a5dd
Murièle Trentini <muriele@trentini.ch>
2017-12-04 12:55:48 +0100

    add watchdog test

---
+++ src/main/java/gvs/tester/WatchdogTest.java
@@ -0,0 +1,26 @@
+package gvs.tester;
+
+import java.util.List;
+
+import gvs.graph.GVSGraph;
+import gvs.model.graph.TestDefaultVertex;
+import gvs.model.graph.TestDirectedEdge;
+
+public class WatchdogTest {
+  private static List<TestDirectedEdge> edges ;
+
+  public static void main(String[] args) {
+    GVSGraph graph = new GVSGraph("Watchdog test");
+    graph.add(new TestDefaultVertex("watch"));
+    graph.display();
+    createEdge(graph);
+    graph.display();
+    graph.disconnect();
+  }
+
+  private static void createEdge(GVSGraph graph) {
+    TestDirectedEdge e = new TestDirectedEdge(null, null, "dog");
+    edges.add(e);
+  }
+
+}
